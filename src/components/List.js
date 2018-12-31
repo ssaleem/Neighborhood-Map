649fc6c2a30c2b2f6541b50c65f9013baf3989bb
Sara Saleem <8222963+ssaleem@users.noreply.github.com>
2018-12-31 13:45:53 -0500

    Fix alt attribute for venue category icons

--- src/components/List.js
+++ src/components/List.js
@@ -13,7 +13,7 @@ class List extends Component {
 			<ol className="attractions-list">
 				{attractions.map((attraction, index) => (
 					<li key={index} tabIndex="0" onClick={(event => this.handleClick(index))}>
-						<div><img src={attraction.icon} alt={attraction.category.logo} className="cat-logo"/><span>{attraction.title}</span></div>
+						<div><img src={attraction.icon} alt={`${attraction.category} icon`} className="cat-logo"/><span>{attraction.title}</span></div>
 						<div className={`${selectedPlace.title === attraction.title ? "address-show" : "address-hide"}`}>{attraction.address}</div>
 					</li>
 				))}
