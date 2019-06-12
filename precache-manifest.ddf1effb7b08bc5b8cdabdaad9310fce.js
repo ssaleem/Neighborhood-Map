4c130b25f4f667905b1a77da12b1b25c8e88b61e
ssaleem <sara@sarasaleem.com>
2019-06-12 12:28:54 -0400

    Updates

---
+++ precache-manifest.ddf1effb7b08bc5b8cdabdaad9310fce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "544c83dc1d6000d06277",
+    "url": "/Trending-in-Tampa-Bay/static/js/runtime~main.544c83dc.js"
+  },
+  {
+    "revision": "69dcd70c1264f693ca7d",
+    "url": "/Trending-in-Tampa-Bay/static/js/main.69dcd70c.chunk.js"
+  },
+  {
+    "revision": "15f91b24d346209f9e1d",
+    "url": "/Trending-in-Tampa-Bay/static/js/1.15f91b24.chunk.js"
+  },
+  {
+    "revision": "69dcd70c1264f693ca7d",
+    "url": "/Trending-in-Tampa-Bay/static/css/main.a865eb08.chunk.css"
+  },
+  {
+    "revision": "33d7506b97ba6207283fb8597bf04edc",
+    "url": "/Trending-in-Tampa-Bay/index.html"
+  }
+];
