9e033af0d4f31a8bc559f2217b3b0693442ef35a
ssaleem <sara@sarasaleem.com>
2019-06-17 15:49:46 -0400

    Minor code refactoring

--- src/components/List.js
+++ src/components/List.js
@@ -1,26 +1,36 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 class List extends Component {
   static propTypes = {
     selectedPlace: PropTypes.object,
     attractions: PropTypes.array.isRequired,
-    listClick: PropTypes.func.isRequired
-  }
-  render() {
-    const attractions = this.props.attractions
-    const selectedPlace = this.props.selectedPlace
+    listClick: PropTypes.func.isRequired,
+  };
+  render () {
+    const {selectedPlace, attractions, listClick} = this.props;
     return (
       <ol className="attractions-list">
-      {attractions.map((attraction, index) => (
-        <li key={index} tabIndex="0" onClick={() => this.props.listClick(index)}>
-          <div><img src={attraction.icon} alt={`${attraction.category} icon`} className="cat-logo"/><span>{attraction.title}</span></div>
-          <div className={`${selectedPlace.title === attraction.title ? "address-show" : "address-hide"}`}>{attraction.address}</div>
-        </li>
-      ))}
+        {attractions.map ((attraction, index) => (
+          <li key={index} tabIndex="0" onClick={() => listClick (index)}>
+            <div>
+              <img
+                src={attraction.icon}
+                alt={`${attraction.category} icon`}
+                className="cat-logo"
+              />
+              <span>{attraction.title}</span>
+            </div>
+            <div
+              className={`${selectedPlace.title === attraction.title ? 'address-show' : 'address-hide'}`}
+            >
+              {attraction.address}
+            </div>
+          </li>
+        ))}
       </ol>
-      )
+    );
   }
 }
 
-export default List
+export default List;
